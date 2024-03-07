042e71cc4c96d68e6f351ac59442f4a3ab40bb8e
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-03-07 14:24:45 -0300

    verceljson

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+const { Pool } = require('pg');
 
 dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL ,
+})
 
 export const sequelize = new Sequelize(
     process.env.PG_DB as string,
