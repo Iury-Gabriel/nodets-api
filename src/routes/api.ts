cf6cd28cd83639c807584ad9681c28b55d7a61a0
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-03-07 14:12:42 -0300

    initial commit

---
+++ src/routes/api.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response } from "express";
+
+import * as apiController from "../controllers/apiController";
+
+const router = Router();
+
+router.get("/ping", apiController.ping);
+
+router.get('/random', apiController.random)
+
+router.get('/nome/:nome', apiController.nome);
+
+router.post('/frases', apiController.createPhrase)
+router.get('/frases', apiController.listPhrases)
+router.get('/frase/aleatoria', apiController.randomPhrase);
+router.get('/frase/:id', apiController.getPhrase)
+router.put('/frase/:id', apiController.updatePhrase)
+router.delete('/frase/:id', apiController.deletePhrase)
+
+export default router;
